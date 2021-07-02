107cfe80a864772e56d60c14a6ab9f6ab718d15c
Leon Ren <yuren1248@163.com>
2021-07-02 14:24:51 +0800

    Add contents about leon

---
+++ src/components/AboutMe/index.js
@@ -0,0 +1,11 @@
+import "./index.scss";
+export default function AboutMe() {
+  return (
+    <section class="about-me">
+      <h3>about me</h3>
+      <p class="description">
+        我是Leon，来自于山东，是一名还算阳光的男生。我喜欢技术，喜欢数理，喜欢比较偏底层的程序设计。我熟悉C、Java、MATLAB、Python等语言，能够写出比较高效的代码。同时也有还说得过去的数理基础。
+      </p>
+    </section>
+  );
+}
---
+++ src/components/Header/index.js
@@ -0,0 +1,17 @@
+import "./index.scss";
+export default function Header() {
+  return (
+    <header className="header">
+      <img
+        className="avatar"
+        alt="Avatar"
+        src="https://github.com/LeonRen1248/training-app/blob/master/Alan_head_icon_bao.jpg"
+        // src="https://inews.gtimg.com/newsapp_match/0/3581582328/0"
+      />
+      <h2>
+        姓名：<span className="name">Leon Ren</span>，年龄：
+        <span className="age">23</span>岁
+      </h2>
+    </header>
+  );
+}
