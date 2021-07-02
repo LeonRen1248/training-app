a946465246394e747a785c4b3880e65e0e8fde4b
Leon Ren <yuren1248@163.com>
2021-07-02 14:37:00 +0800

    change head icon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,8 +5,8 @@ export default function Header() {
       <img
         className="avatar"
         alt="Avatar"
-        src="https://github.com/LeonRen1248/training-app/blob/master/Alan_head_icon_bao.jpg"
-        // src="https://inews.gtimg.com/newsapp_match/0/3581582328/0"
+        // src="https://github.com/LeonRen1248/training-app/blob/master/Alan_head_icon_bao.jpg"
+        src="https://inews.gtimg.com/newsapp_match/0/3581582328/0"
       />
       <h2>
         姓名：<span className="name">Leon Ren</span>，年龄：
