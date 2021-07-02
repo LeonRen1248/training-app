171497fbec646323c86a8dc6c2be5fa90a49487d
Leon Ren <yuren1248@163.com>
2021-07-02 15:16:05 +0800

    Change head icon to Bao.jpg

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,9 +5,10 @@ export default function Header() {
       <img
         className="avatar"
         alt="Avatar"
-        src="https://imgtu.com/i/R6hOPK"
+        src="https://z3.ax1x.com/2021/07/02/R6hOPK.jpg"
         // src="https://github.com/LeonRen1248/training-app/blob/master/Alan_head_icon_bao.jpg"
         // src="https://inews.gtimg.com/newsapp_match/0/3581582328/0"
+        // <a href="https://imgtu.com/i/R6hOPK"><img src="https://z3.ax1x.com/2021/07/02/R6hOPK.jpg" alt="R6hOPK.jpg" border="0" /></a>
       />
       <h2>
         姓名：<span className="name">Leon Ren</span>，年龄：
